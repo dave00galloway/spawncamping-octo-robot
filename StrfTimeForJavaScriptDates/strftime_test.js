73e78397ec7737ec5794f25710d67bd66e843f34
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-02-24 08:19:10 +0000

    added test result output (to html page)

--- StrfTimeForJavaScriptDates/strftime_test.js
+++ StrfTimeForJavaScriptDates/strftime_test.js
@@ -7,6 +7,13 @@ function assert(message, expr) {
 }
 assert.count = 0;
 
+function output(text, color) {
+	var p = document.createElement("p");
+	p.innerHTML = text;
+	p.style.color = color;
+	document.body.appendChild(p);
+}
+
 var date = new Date(2009, 11, 5);
 try {
 	assert("%Y should return full year", date.strftime("%Y") === "2009");
@@ -14,7 +21,7 @@ try {
 	assert("%d should return date", date.strftime("%d") === "02");
 	assert("%y should return year as two digits", date.strftime("%y") === "09");
 	assert("%F should act as %Y-%m-%d", date.strftime("%F") === "2009-10-02");
-	console.log(assert.count + " tests OK");
+	output(assert.count + " tests OK", "#0c0");
 } catch (e) {
-	console.log("Test failed: " + e.message);
+	output("Test failed: " + e.message, "#c00");
 }
