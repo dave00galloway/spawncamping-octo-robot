da391640475142d9a3455115a2bcb06090817f5a
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-02-26 08:26:55 +0000

    benchmaking test done

--- TestToLearn/test/array_test.js
+++ TestToLearn/test/array_test.js
@@ -1,9 +1,12 @@
 TestCase("ArrayTest", {
-	"test array splice should not modify array" : function() {
-		var arr = [ 1, 2, 3, 4, 5 ];
-		var result = arr.splice(2, 3);
-		assert.equal([ 1, 2, 3, 4, 5 ], arr);
-	}
-}
-
-);
+  "test array splice should modify array": function () {
+    var arr = [1, 2, 3, 4, 5];
+    var result = arr.splice(2, 3);
+    assertEquals([1, 2, ], arr);
+  },
+  "test array splice returns removed items from array" :function(){
+	    var arr = [1, 2, 3, 4, 5];
+	    var result = arr.splice(2, 3);
+	    assertEquals([3,4,5], result);	  
+  }
+});
